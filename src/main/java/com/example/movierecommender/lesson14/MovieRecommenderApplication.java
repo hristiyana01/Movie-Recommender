9f5d0e831a32fa912a08fca4f44c221650659d16
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-08-05 10:51:44 +0300

    replacing annotations with XML context

--- src/main/java/com/example/movierecommender/lesson14/MovieRecommenderApplication.java
+++ src/main/java/com/example/movierecommender/lesson14/MovieRecommenderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
@@ -16,13 +17,16 @@ public class MovieRecommenderApplication {
         ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
         RecommenderImplementation recommender = appContext.getBean("recommenderImplementation",RecommenderImplementation.class);
 
+        //check the beans which have been loaded
+        System.out.println("\nBeans loaded:");
+        System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
         //call method to get recommendations
         String[] result = recommender.recommendMovies("Finding Dory");
 
         //display results
         System.out.println(Arrays.toString(result));
 
-       // appContext.close();
+        appContext.close();
     }
 
 }
